6662ccb54899831ef2f98a9cad070ce3f67c60aa
Harendrasinh <haren111990@gmail.com>
2025-04-18 21:30:41 +0530

    vadang

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,124 +3,131 @@ import { Carousel } from './Carousel'
 import { motion } from 'framer-motion'
 import "../src/index.css"
 export const Content = () => {
-    
+
     return (
-            <div className='h-[calc(100vh-10rem)]flex flex-col justify-center items-center gap-5'>
-                <div className='overflow-hidden h-[15rem] w-screen flex justify-center items-center relative'>
-                    <motion.div
-                        className="absolute top-0"
-                        initial={{ y: "30%" }}
-                        animate={{ y: "-50%" }}
-                        transition={{
-                            duration: 10,
-                            repeat: Infinity,
-                            ease: "linear",
-                        }}
-                    >
-                        <div className='flex flex-col justify-center items-center space-y-1'>
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    प्रकाशन सोहळ्याचे अध्यक्ष
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    मा. दादासाहेब डॉ. संजयकुमारजी आर. देवरे (नाक, कान, घसा शल्यचिकित्सक)<br />
-                                    अध्यक्ष-आ.ध.मा.शि.प्र.संस्था, धुळे
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    मा. प्राचार्य तानसेन जगताप
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    संस्थापक अध्यक्ष-म.सा.प.शाखा-४०गाव
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    मा.प्रा.डॉ.आशुतोष पाटील
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    समीक्षक,संचालक-आजीवन अध्यापन व विस्तार विभाग क.ब.चौ.उ.म.वि.जळगाव
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    मा.प्राचार्य डॉ.दिलीप धोङगे
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    समीक्षक,संत साहित्याचे गाढे अभ्यासक
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    मा.युवराज माळी
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    संचालक, अथर्व पब्लिकेशन, जळगाव
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className='flex flex-col justify-center items-center space-y-1'>
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    प्रकाशन सोहळ्याचे अध्यक्ष
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    मा. दादासाहेब डॉ. संजयकुमारजी आर. देवरे (नाक, कान, घसा शल्यचिकित्सक)<br />
-                                    अध्यक्ष-आ.ध.मा.शि.प्र.संस्था, धुळे
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    मा. प्राचार्य तानसेन जगताप
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    संस्थापक अध्यक्ष-म.सा.प.शाखा-४०गाव
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    मा.प्रा.डॉ.आशुतोष पाटील
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    समीक्षक,संचालक-आजीवन अध्यापन व विस्तार विभाग क.ब.चौ.उ.म.वि.जळगाव
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    मा.प्राचार्य डॉ.दिलीप धोङगे
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    समीक्षक,संत साहित्याचे गाढे अभ्यासक
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col items-center justify-center text-center">
-                                <div className="text-[1rem] font-bold">
-                                    मा.युवराज माळी
-                                </div>
-                                <div className="text-[1rem] w-[20rem]">
-                                    संचालक, अथर्व पब्लिकेशन, जळगाव
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </motion.div>
-                </div>
+        <div className='h-[calc(120vh-10rem)] flex flex-col justify-center items-center '>
+            <div className='text-[2rem] text-amber-900 font-black'>वडंग</div>
+            <div className='flex justify-center items-center  text-amber-900 mb-3'>
                 <div className='flex flex-col justify-center items-center'>
-                    <div className='text-amber-950 font-bold tiro-devanagari-marathi-regular-italic'>लेखक संजीव गिरास</div>
-                    <div><Carousel /></div>
+                    <div className='font-bold'>दिनांक वेळ</div>
+                    <div>रविवार,२० एप्रिल २०२५</div>
                 </div>
             </div>
-        
+            <div className='overflow-hidden h-[15rem] w-screen flex justify-center items-center relative mb-6'>
+                <motion.div
+                    className="absolute top-0"
+                    initial={{ y: "30%" }}
+                    animate={{ y: "-50%" }}
+                    transition={{
+                        duration: 10,
+                        repeat: Infinity,
+                        ease: "linear",
+                    }}
+                >
+                    <div className='flex flex-col justify-center items-center space-y-1'>
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                प्रकाशन सोहळ्याचे अध्यक्ष
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                मा. दादासाहेब डॉ. संजयकुमारजी आर. देवरे (नाक, कान, घसा शल्यचिकित्सक)<br />
+                                अध्यक्ष-आ.ध.मा.शि.प्र.संस्था, धुळे
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                मा. प्राचार्य तानसेन जगताप
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                संस्थापक अध्यक्ष-म.सा.प.शाखा-४०गाव
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                मा.प्रा.डॉ.आशुतोष पाटील
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                समीक्षक,संचालक-आजीवन अध्यापन व विस्तार विभाग क.ब.चौ.उ.म.वि.जळगाव
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                मा.प्राचार्य डॉ.दिलीप धोङगे
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                समीक्षक,संत साहित्याचे गाढे अभ्यासक
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                मा.युवराज माळी
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                संचालक, अथर्व पब्लिकेशन, जळगाव
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='flex flex-col justify-center items-center space-y-1'>
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                प्रकाशन सोहळ्याचे अध्यक्ष
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                मा. दादासाहेब डॉ. संजयकुमारजी आर. देवरे (नाक, कान, घसा शल्यचिकित्सक)<br />
+                                अध्यक्ष-आ.ध.मा.शि.प्र.संस्था, धुळे
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                मा. प्राचार्य तानसेन जगताप
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                संस्थापक अध्यक्ष-म.सा.प.शाखा-४०गाव
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                मा.प्रा.डॉ.आशुतोष पाटील
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                समीक्षक,संचालक-आजीवन अध्यापन व विस्तार विभाग क.ब.चौ.उ.म.वि.जळगाव
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                मा.प्राचार्य डॉ.दिलीप धोङगे
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                समीक्षक,संत साहित्याचे गाढे अभ्यासक
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center text-center">
+                            <div className="text-[1rem] font-bold">
+                                मा.युवराज माळी
+                            </div>
+                            <div className="text-[1rem] w-[20rem]">
+                                संचालक, अथर्व पब्लिकेशन, जळगाव
+                            </div>
+                        </div>
+
+                    </div>
+
+                </motion.div>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+                <div className='text-amber-950 font-bold tiro-devanagari-marathi-regular-italic'>लेखक संजीव गिरास</div>
+                <div><Carousel /></div>
+            </div>
+        </div>
+
     )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,13 +3,15 @@ import { motion } from 'framer-motion'
 export const Header = () => {
     return (
         <div className='h-[10rem] flex flex-col justify-center items-center w-screen'>
-            <motion.div className='text-amber-950 text-[1.2rem] font-bold'
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 2 }}
-            >
-                "कादंबरी प्रकाशन सोहळा"
-            </motion.div>
+            <div className='flex  justify-center items-center'>
+                <img className='w-[3rem] h-[3rem]' src="images/bapu/bapu1.png" alt="bapupng" />
+                <motion.div className='text-amber-950 text-[1.2rem] font-bold'
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 2 }}>
+                    "कादंबरी प्रकाशन सोहळा"
+                </motion.div>
+            </div>
             <motion.div className='text-amber-950 text-[0.8rem] flex flex-col justify-center items-center'>
                 <div>विठाई प्रतिष्ठान,ठाणे व स्व.अण्णासाहेब आर.डी.देवरे कला व </div>
                 <div>विज्ञान महाविद्यालय,म्हसदी,ता.साक्री,जि.धुळे यांचे संयुक्त विद्यमाने आयोजित</div>
