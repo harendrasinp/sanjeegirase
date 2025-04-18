0e184489a32fa3f4ff80aeabf1785352ddb16854
Harendrasinh <haren111990@gmail.com>
2025-04-18 17:30:56 +0530

    add music

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Layout } from './layout/Layout'
 import { Content } from './Content'
 export const App = () => {
   return (
-    <div className='bg-[url(/images/bg/background.jpg)] bg-fixed bg-cover bg-no-repeat bg-center'>
+    <div className=''>
       <Layout>
           <Content/>
       </Layout>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Footer } from './Footer'
 import { Header } from './Header'
-export const Layout = ({children}) => {
-  return (
-    <div>
-        <Header/>
-            <main>
-                {children}
-            </main>
-        <Footer/>
-    </div>
-  )
+import { useRef } from 'react'
+import "../../src/index.css"
+export const Layout = ({ children }) => {
+    const [toggle, setToogle] = useState(false);
+    const musicRef = useRef();
+    const handlePlay = () => {
+        musicRef.current.play();
+        setToogle(true)
+    };
+    return (
+        <div className=' flex flex-col gap-6'>
+            <div className={`bg-[url(/images/bg/background.jpg)] bg-fixed bg-cover bg-no-repeat bg-center overflow-y-hidden ${toggle ? "animation" : "h-[10rem]"}`}>
+                <audio ref={musicRef} src="music/backmusic.mp3"></audio>
+                <Header />
+                <main>
+                    {children}
+                </main>
+                <Footer />
+            </div>
+            {toggle ? null
+                :<div className='flex justify-center items-center'>
+                    <button className='bg-amber-900 text-amber-50 rounded-[0.5rem] px-5 py-2' onClick={handlePlay}>Open</button>
+                </div>
+            }
+        </div>
+    )
 }
